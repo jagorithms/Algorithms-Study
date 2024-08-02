a139450dadcbf8e61f3743a2c46e9a5b00aefcff
okxooxoo <okxooxoo@naver.com>
2024-08-02 15:01:09 +0900

    feat: 오큰수 #17298

--- #5397 키로거/김서로.java
+++ #5397 키로거/김서로.java
@@ -13,31 +13,31 @@ public class 김서로 {
 		for (int i=0; i<T; i++) {
 			String password = input.readLine();
 			Stack<Character> leftStack = new Stack<>();
-		    	Stack<Character> rightStack = new Stack<>();
-		    
-		    	for (char c: password.toCharArray()) {
-				if (Character.isLetterOrDigit(c)) {
-					leftStack.add(c);
-				} else if (!(leftStack.isEmpty()) && c == '<') {
-					rightStack.add(leftStack.pop());
-				} else if (!(rightStack.isEmpty()) && c == '>') {
-					leftStack.add(rightStack.pop());
-				} else if (!(leftStack.isEmpty()) && c == '-') {
-					leftStack.pop();
-				}
-		    	}
-		    
-		    	while (!(rightStack.isEmpty())) {
-				leftStack.add(rightStack.pop());
-		    	}
-		    
-		    	StringBuilder output = new StringBuilder();
-		    
-		    	for (char c: leftStack) {
-				output.append(c);
-		    	}
-		    
-		    	System.out.println(output);
+            Stack<Character> rightStack = new Stack<>();
+            
+            for (char c: password.toCharArray()) {
+            	if (Character.isLetterOrDigit(c)) {
+            		leftStack.add(c);
+            	} else if (!(leftStack.isEmpty()) && c == '<') {
+            		rightStack.add(leftStack.pop());
+            	} else if (!(rightStack.isEmpty()) && c == '>') {
+            		leftStack.add(rightStack.pop());
+            	} else if (!(leftStack.isEmpty()) && c == '-') {
+            		leftStack.pop();
+            	}
+            }
+            
+            while (!(rightStack.isEmpty())) {
+            	leftStack.add(rightStack.pop());
+            }
+            
+            StringBuilder output = new StringBuilder();
+            
+            for (char c: leftStack) {
+            	output.append(c);
+            }
+            
+            System.out.println(output);
 		}
 	}
 }
