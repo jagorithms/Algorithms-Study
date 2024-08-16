85f7f68fbfe61285abf1e7a6f85fdef3f6a686b8
becooq81 <77732679+becooq81@users.noreply.github.com>
2024-08-16 10:50:00 +0900

    Update 강희진.java

--- #2003 수들의 합 2/강희진.java
+++ #2003 수들의 합 2/강희진.java
@@ -1,1 +1,41 @@
+import java.io.*;
+import java.util.*;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+
+        int[] nums = new int[N+1];
+        int idx = 0;
+        while (st.hasMoreTokens()) {
+            nums[1+idx++] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] dp = new int[N+1];
+
+        for (int i = 1; i <= N; i++) {
+            dp[i] = nums[i] + dp[i-1];
+        }
+        int count = 0;
+
+        for (int i = 0; i <= N; i++) {
+            for (int j = i+1; j <= N; j++) {
+                if (dp[j] - dp[i] == M) count++;
+            }
+        }
+        sb.append(count);
+        bw.write(sb.toString());
+        bw.flush();
+        br.close();
+    }
+
+    
+}
