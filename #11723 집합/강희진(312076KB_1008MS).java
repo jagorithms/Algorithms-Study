df0a2bcc1c5660d5afc2c1706425d65b284acbc3
becooq81 <77732679+becooq81@users.noreply.github.com>
2024-08-27 12:54:30 +0900

    Update 강희진(312076KB_1008MS).java

--- #11723 집합/강희진(312076KB_1008MS).java
+++ #11723 집합/강희진(312076KB_1008MS).java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int flag = 0;;
+    static int flag = 0;
     static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
