68b792b3cf65e81daf06c32ff9682ed5d1e45668
becooq81 <becooq81@gmail.com>
2024-09-18 17:21:47 +0900

    feat: clean up main branch

--- #1158 요세푸스 문제/김서로.py
+++
@@ -1,8 +0,0 @@
-N, K = map(int, input().split())
-lst = [i + 1 for i in range(N)] # 1부터 N까지의 숫자로 이루어진 리스트 생성
-josephus = []
-index = 0
-for _ in range(N):
-    index = (index + K - 1) % len(lst)
-    josephus.append(lst.pop(index))
-print("<" + ", ".join(map(str, josephus)) + ">")
