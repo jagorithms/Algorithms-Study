8cfced1fcbe2b9669c0b17c9f6b045e4d542e3e5
rnqhscjf3333 <41632370+rnqhscjf3333@users.noreply.github.com>
2024-10-29 17:17:01 +0900

    Update 구본관.sql

--- 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/구본관.sql
+++ 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/구본관.sql
@@ -1,1 +1,35 @@
+with sub1 as (
+    select car_id, discount_rate
+    from CAR_RENTAL_COMPANY_DISCOUNT_PLAN p join CAR_RENTAL_COMPANY_CAR c
+    on p.car_type = c.car_type
+    where duration_type = "30일 이상" and (p.car_type = "세단" or p.car_type = "SUV")
+),
+sub2 as (
+    select car_id
+    from CAR_RENTAL_COMPANY_RENTAL_HISTORY  
+    where not (end_date < "2022-11-01" or start_date > "2022-11-30")
+)
+select c.car_id, car_type, round(daily_fee * 30 * (100 - discount_rate) / 100, 0) as FEE
+from CAR_RENTAL_COMPANY_CAR c
+join sub1 on c.car_id = sub1.car_id
+where c.car_id not in (select * from sub2)
+and 500000 <= round(daily_fee * 30 * (100 - discount_rate) / 100, 0) 
+and round(daily_fee * 30 * (100 - discount_rate) / 100, 0) < 2000000
+order by FEE desc, car_type, c.car_id desc;
 
+
+
+
+/* 
+CAR_RENTAL_COMPANY_CAR
+car_id	car_type	daily_fee	     options
+   1	   트럭	     102000	  주차감지센서,열선시트
+
+CAR_RENTAL_COMPANY_RENTAL_HISTORY
+history_id	car_id	    start_date	         end_date
+    506	      15	2022-08-01 00:00:00	2022-08-02 00:00:00
+
+CAR_RENTAL_COMPANY_DISCOUNT_PLAN 
+plan_id	car_type	duration_type	discount_rate
+    1	  세단	      7일 이상	          5
+*/
