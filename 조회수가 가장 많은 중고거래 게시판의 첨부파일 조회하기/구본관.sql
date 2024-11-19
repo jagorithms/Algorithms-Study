a6f2aabf8de00729bad7d7cf89ad6cf00bbc239d
rnqhscjf3333 <41632370+rnqhscjf3333@users.noreply.github.com>
2024-11-19 09:35:25 +0900

    Update 구본관.sql

--- 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/구본관.sql
+++ 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/구본관.sql
@@ -1,1 +1,8 @@
-
+SELECT CONCAT('/home/grep/src/', board.BOARD_ID, '/', FILE_ID, FILE_NAME, FILE_EXT) AS FILE_PATH
+FROM USED_GOODS_BOARD as board, USED_GOODS_FILE as file
+where board.BOARD_ID = file.BOARD_ID and board.VIEWS = (
+    SELECT MAX(VIEWS)
+    FROM USED_GOODS_BOARD
+)
+ORDER BY FILE_ID DESC
+;
